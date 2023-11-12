3f405fcd32ed5af5959917b7b24aefcd2b5e40f7
cyrilRam <119733403+cyrilRam@users.noreply.github.com>
2023-11-12 18:00:41 +0100

    boutto n save

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const Table = () => {
     const [data, setData] = useState([]);
     const [dataLoad, setDataLoad] = useState(false);
+    const [modifyData, setModifyData] = useState(false)
 
     const getData = () => {
         axios.get(
@@ -16,6 +17,34 @@ const Table = () => {
 
     }
 
+    const postNewData = async () => {
+        if (modifyData) {
+            console.log(data)
+            try {
+                // Effectuez la requête POST
+                const response = await axios.post(
+                    `http://localhost:8000/api/formations/updateTable`,
+                    data,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                );
+
+                // Gérez la réponse de l'API ici
+                alert("Réponse de l'API :" + response.data);
+            } catch (error) {
+                console.error("Erreur lors de l'envoi du fichier :", error);
+                const errorDetails = error.response.data.detail;
+                console.log(errorDetails);
+                alert("Erreur : " + errorDetails);
+            }
+        } else {
+            alert("Acune Modification n'a été effectuée");
+        }
+    }
+
     useEffect(() => {
         getData();
         console.log(data);
@@ -25,7 +54,8 @@ const Table = () => {
         const newData = [...data];
         newData[index][fieldName] = value;
         setData(newData);
-        console.log(data)
+        console.log(data);
+        setModifyData(true);
 
     };
 
@@ -34,13 +64,31 @@ const Table = () => {
             const newData = [...prevData];
             newData.splice(index, 1);
             console.log(newData); // newData est le tableau mis à jour
+            setModifyData(true);
+
             return newData;
         });
     };
 
+    const handleAdd = () => {
+        const newDataRow = {}; // Create an empty object for the new row
+        columns.forEach(column => {
+            newDataRow[column] = ""; // You can set a default value here if needed
+        });
+
+        // Add the new row to the state
+        setData(prevData => [...prevData, newDataRow]);
+        setModifyData(true);
+
+
+    }
+
+
     const columns = data[0] ? Object.keys(data[0]) : [];
     return (
         <div>
+            <button onClick={postNewData}>Save</button>
+            <button onClick={handleAdd}>Add Row</button>
             {columns.length > 0 && (
                 <table>
                     <thead>
